e9f02ac5068e3697dc4de0787c6cca187f9fb134
mithilazende <mithilazende@gmail.com>
2023-03-03 02:13:27 +0530

    write search pages. flipbook carousasl adjustment

--- assets/js/curiosity-zone.js
+++ assets/js/curiosity-zone.js
@@ -144,3 +144,9 @@ async function getJokes() {
 getInspiQuote();
 getRiddles();
 getJokes();
+
+var myCarousel = document.querySelector('#carouselExampleCaptions')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 5000,
+  wrap:false
+})
